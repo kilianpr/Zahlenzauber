29234081e3e52fae811d0f651bdbeaa50f602ea1
kilianpr <court.killer1.0@gmail.com>
2021-09-19 14:55:46 +0200

    Continued the ParticleSystem Tutorial of SimonDev

--- src/particles.js
+++ src/particles.js
@@ -1,5 +1,32 @@
 import fire from '/res/particles/fire.png'
 import water from '/res/particles/water.png'
+
+
+const _VS = `
+uniform float pointMultiplier;
+attribute float size;
+attribute float angle;
+attribute vec4 colour;
+varying vec4 vColour;
+varying vec2 vAngle;
+void main() {
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * mvPosition;
+  gl_PointSize = size * pointMultiplier / gl_Position.w;
+  vAngle = vec2(cos(angle), sin(angle));
+  vColour = colour;
+}`;
+
+const _FS = `
+uniform sampler2D diffuseTexture;
+varying vec4 vColour;
+varying vec2 vAngle;
+void main() {
+  vec2 coords = (gl_PointCoord - 0.5) * mat2(vAngle.x, vAngle.y, -vAngle.y, vAngle.x) + 0.5;
+  gl_FragColor = texture2D(diffuseTexture, coords) * vColour;
+}`;
+
+
 class ParticleSystem{
     constructor(params){
         const uniforms = {
@@ -9,7 +36,20 @@ class ParticleSystem{
             pointMultiplier: {
                 value: window.innerHeight / (2.0 * Math.tan(0.5 * 60.0 * Math.PI / 180.0))
             }
-        }
+        };
+        this._material = new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            vertexShader: _VS,
+            fragmentShader: _FS,
+            blending: THREE.AdditiveBlending,
+            depthTest: true,
+            depthWrite: false,
+            transparent: true,
+            vertexColors: true
+        });
+        this._camera = params.camera;
+        this._particles = [];
+        this._geometry = new THREE.BufferGeometry();
     }
 
 }
